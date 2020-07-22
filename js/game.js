3df6cef505084b2d181c404465b8fdcbf80b3182
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-07-22 23:21:26 +0530

    commit

--- js/game.js
+++ js/game.js
@@ -64,10 +64,7 @@ class Game{
                          
                      }
                     
-                         textSize(25);
-                         fill("white");
-                         text("Player 1 :" +allPlayers.player1.score,50,50);
-                        text("Player 2 :" + allPlayers.player2.score, 50, 100);
+                     
                  
                  }
                 
@@ -104,15 +101,7 @@ class Game{
                  }
                  
                   if (player.index !== null) {
-                      for (var i = 0; i < fruitGroup.length; i++) {
-                          if (fruitGroup.get(i).isTouching(players)) {
-                              fruitGroup.get(i).destroy();
-                              player.score =player.score+1;
-                              player.update();
-                              
-                          }
-                          
-                      }
+                     //fill code here, to destroy the objects.
                   }
                 
 
